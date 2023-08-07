1acbd2e5802c298b596413bff72e9d488cd508d7
Muneeb912-cmd <81864431+Muneeb912-cmd@users.noreply.github.com>
2023-08-07 22:02:05 +0500

    Initial Commit
    Developed Dash Board

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import { MyProSidebarProvider } from "./Pages/SideBar/sidebarContext";
+import Topbar from "./Pages/TopBar/Topbar";
+import Dashboard from "./Pages/Dashboard/index";
+import Test from './Pages/Test Screen/test'
+
+const App = () => {
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <MyProSidebarProvider>
+          <div style={{ height: "100%", width: "100%" }}>
+            <main>
+              <Topbar />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />                
+                <Route path="/test" element={<Test />} />                
+              </Routes>
+            </main>
+          </div>
+        </MyProSidebarProvider>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+};
+
+export default App;
